1db0ea82c797f15e64acda61a781b5da41985c00
Randall Frisk <friskslalom6@gmail.com>
2016-04-04 05:39:34 -0700

    test out deploy on changing app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var PORT = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   console.log('%d request received', process.pid);
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello world!\n');
+  res.end('Hello Frisky!\n');
 }).listen(PORT);
 
 console.log('%d listening on %d', process.pid, PORT);
